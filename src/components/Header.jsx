c632decd3256d2a4a42f1000def92d5032aef739
Faith Makinde <faithmakinde0@gmail.com>
2023-04-28 00:48:15 +0100

    header section almost complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <div className='flex justify-between items-center mx-2 max-w-7xl sm:mx-auto'>
+      <div className='flex items-center'>
+        <MenuItem title='HOME' address='/' Icon={AiFillHome}/>
+        <MenuItem title='ABOUT' address='/about' Icon={BsFillInfoCircleFill}/>
+      </div>
+      <div>
+        <Link href='/'>
+            <h2 className='text-white'><span className='bg-amber-500 font-bold px-3 py-1 rounded-lg mr-1'>IMDb</span><span className='text-xl hidden sm:inline'>Clone</span></h2>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const MenuItem = ({title, address, Icon}) => {
+  return (
+    <div>       
+      <Link href={address} className='mx-4 lg:mx-6 hover:text-amber-600'>
+        <Icon size={30} className='sm:hidden mx-4'/>
+        <p className='hidden sm:inline my-2  text-[14px]'>{title}</p></Link>
+    </div>
+  )
+}
+
+export default MenuItem
