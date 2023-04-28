03daec27c006589cb9c9fae30e95170e47e315ef
Faith Makinde <faithmakinde0@gmail.com>
2023-04-29 00:37:39 +0100

    fetch data from tmdb api in the homepage

---
+++ src/components/Results.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Results({results}) {
+  return (
+    <div>
+      {results.map(result => {
+        return(
+            <div key={result.id}>
+               {result.original_title}
+            </div>
+        )
+      })}
+    </div>
+  )
+}
