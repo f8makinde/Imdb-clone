00965ed1c86dd8e7f028039258d16820847371b6
Faith Makinde <faithmakinde0@gmail.com>
2023-04-29 01:03:10 +0100

    added loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+  
+<div class="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 ">
+    <div class="border-t-transparent border-solid animate-spin  rounded-full border-amber-400 border-8 h-16 w-16"></div>
+</div>
+  )
+}
+
+export default loading
