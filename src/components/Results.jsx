33873128114f41d1c17aac6d01f09230fa00e1ea
Faith Makinde <faithmakinde0@gmail.com>
2023-04-30 15:20:22 +0100

    added card component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,15 @@
-
-
+import Card from "./Card"
 export default function Results({results}) {
   return (
-    <div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5 max-w-7xl mx-auto py-4">
       {results.map(result => {
         return(
-            <div key={result.id}>
-               {result.original_title}
-            </div>
+      
+  <Card key={result.id} result={result}/>
+          
+            // <div key={result.id}>
+            //    {result.original_title}
+            // </div>
         )
       })}
     </div>
